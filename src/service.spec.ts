0a46d65579c232cd5fdfb2a3949b658b9e8a9937
eigidi <agdumitrascu@yahoo.com>
2024-06-25 23:02:57 +0200

    project creation

---
+++ src/service.spec.ts
@@ -0,0 +1,12 @@
+import calc from './service';
+
+describe('Calc', () => {
+  
+  test('should ...', () => {
+    const input = 0;
+    const expected = "";
+    expect(calc.generateFizzBuzz(input)).toBe(expected);
+  });
+
+
+});
---
+++ src/service.ts
@@ -0,0 +1,23 @@
+class Service {
+  /**
+ * Generates the following:
+ * If the number is divisible by 3, it returns 'Fizz'.
+ * If the number is divisible by 5, it returns 'Buzz'.
+ * If the number is divisible by both 3 and 5, it returns 'FizzBuzz'.
+ * If none of the above conditions are met, it returns the message "Not a FB".
+ * 
+ * @param {number} number - The input number.
+ * @returns {string} The FizzBuzz value corresponding to the input number.
+ * 
+ * @example
+ * FizzBuzzService.generateFizzBuzz(3); // => 'Fizz'
+ * FizzBuzzService.generateFizzBuzz(5); // => 'Buzz'
+ * FizzBuzzService.generateFizzBuzz(15); // => 'FizzBuzz'
+ * FizzBuzzService.generateFizzBuzz(7); // => 'Not a FB'
+ */
+  generateFizzBuzz(a: number): any {
+    return "";
+  }
+}
+
+export default new Service();
