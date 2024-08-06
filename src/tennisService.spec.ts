6242b6bdd114635a9a913d4739a142b7d8c421ad
eigidi <agdumitrascu@yahoo.com>
2024-08-06 14:07:57 +0200

    mob prog tennis

--- src/tennisService.spec.ts
+++ src/tennisService.spec.ts
@@ -2,7 +2,49 @@ import tennisService from './tennisService';
 
 describe('Tennis score', () => {
 
-  test('should return Deuce', () => {
+  test('should return Love all when score is 0-0', () => {
+    expect(tennisService.getScore()).toBe("Love all");
+  });
+
+  test('should return Fifteen all when score is 15-15', () => {
+    tennisService.setScore(["15", "15"]);
+
+    expect(tennisService.getScore()).toBe("Fifteen all");
+  });
+
+  test('should return Love, Fifteen when score is 0-15', () => {
+    tennisService.setScore(["0", "15"]);
+
+    expect(tennisService.getScore()).toBe("Love, Fifteen");
+  });
+
+  test('should return Deuce when score is 40-40', () => {
+    tennisService.setScore(["40", "40"]);
+
     expect(tennisService.getScore()).toBe("Deuce");
+  });
+
+  test('should return AD-40 when score is AD-40', () => {
+    tennisService.setScore(["AD", "40"]);
+
+    expect(tennisService.getScore()).toBe("Advantage Player One");
+  });
+
+  test('should return 40-AD when score is 40-AD', () => {
+    tennisService.setScore(["40", "AD"]);
+
+    expect(tennisService.getScore()).toBe("Advantage Player Two");
+  });
+
+  test('should return Player One wins', () => {
+    tennisService.setScore(["W", "30"]);
+
+    expect(tennisService.getScore()).toBe("Player One wins");
+  });
+
+  test('should return Player Two wins', () => {
+    tennisService.setScore(["30", "W"]);
+
+    expect(tennisService.getScore()).toBe("Player Two wins");
   });
 });
--- src/tennisService.ts
+++ src/tennisService.ts
@@ -1,4 +1,19 @@
 class Service {
+  player1: {
+    name: string,
+    score: string
+  } = {
+    name: "Player One",
+    score: "0",
+  };
+  player2: {
+    name: string,
+    score: string
+  } = {
+    name: "Player Two",
+    score: "0",
+  };
+
   /**
    * Write a program that implements a simple tennis-style scoring announcements system.
    * A game is won by the first player to have won at least four points in total and at least two points more than the opponent.
@@ -13,9 +28,59 @@ class Service {
  * If Player one has an advantage, then getScore will return "Advantage Player One"
  * If Player one has won, then getScore will return "Player One wins"
  */
-  getScore(): any {
-    return "Deuce";
+  getScore(): any {   
+    if(this.winner) {
+      return `${this.winner} wins`;
+    }
+
+    if(this.advantage) {
+      return `Advantage ${this.advantage}`;
+    }
+
+    if(this.player1.score === this.player2.score){
+
+      if(this.player1.score === "40"){
+        return "Deuce"
+      }
+
+      return `${scoreMapper[this.player1.score]} all`
+    }
+    
+    return `${scoreMapper[this.player1.score]}, ${scoreMapper[this.player2.score]}`;
   }
+
+  getStatus(status: string) {
+    if(this.player1.score === status) {
+      return this.player1.name;
+    }
+    if(this.player2.score === status) {
+      return this.player2.name;
+    }
+
+    return null; 
+  }
+
+  get winner(){
+    return this.getStatus("W"); 
+  }
+
+  get advantage(){
+    return this.getStatus("AD"); 
+  }
+
+  setScore(scores: string[]) {
+    const [player1, player2] = scores;
+
+    this.player1.score = player1;
+    this.player2.score = player2;
+  }
+}
+
+const scoreMapper: {[key: string]: string} = {
+  "0": "Love",
+  "15": "Fifteen",
+  "30": "Thirty",
+  "40": "Forty",
 }
 
 export default new Service();
